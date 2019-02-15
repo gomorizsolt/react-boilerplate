251b8fcbb29c1f547065bd95ddf21d4def1b49f7
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-02-15 03:44:26 +0100

    Fix linting error in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   module: {
@@ -7,26 +7,26 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
-    })
+      template: './public/index.html',
+      filename: './index.html',
+    }),
   ],
   devServer: {
     port: 3000,
-  }
+  },
 };
