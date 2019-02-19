1dd13290c7bebf05d6431abf265711336e306a5a
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-02-19 20:52:27 +0100

    Add Heroku deployment scripts and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,36 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ['babel-polyfill', path.join(__dirname, 'src/index.js')],
+  entry: ["@babel/polyfill", path.join(__dirname, "src/index.js")],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
+            loader: "html-loader",
           },
         ],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
+      template: "./public/index.html",
+      filename: "./index.html",
     }),
     new Dotenv(),
   ],
