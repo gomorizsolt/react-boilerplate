83d0864e31b59939036ff68d5b7c0d49a9e258c8
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-02-26 14:39:59 +0100

    Remove unused RB logo import from App.test.js

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 import { BasicDiv, List } from "./App.style";
-import RB from "../../RB.svg";
 
 describe("<App />", () => {
   let wrapper;
