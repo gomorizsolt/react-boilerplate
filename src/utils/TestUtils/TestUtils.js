77a59166454cc5abbb9f9b1612fe5c2a122590a1
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-05-06 08:43:13 +0200

    Fix build error

--- src/utils/TestUtils/TestUtils.js
+++ src/utils/TestUtils/TestUtils.js
@@ -4,7 +4,9 @@ export const mockOriginalFunctionality = name => {
   return {
     ...Object.getOwnPropertyNames(actualModule)
       .map(functionName => ({
-        [functionName]: jest.fn().mockImplementation((...args) => actualModule[functionName](...args)),
+        [functionName]: jest
+          .fn()
+          .mockImplementation((...args) => actualModule[functionName](...args)),
       }))
       .reduce((accumulator, currentValue) => ({ ...accumulator, ...currentValue })),
   };
